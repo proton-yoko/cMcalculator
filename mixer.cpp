8bdd9ab69a13213a15fa6d08de8f30dceaaadf24
proton <whitedew.kilau@gmail.com>
2019-01-01 21:16:01 +0900

    modified

--- mixer.cpp
+++ mixer.cpp
@@ -8,7 +8,6 @@
 #include <vector>
 #include <iomanip>
 #include <cstdlib>
-#include <array>
 #include <random>
 
 using namespace std;
@@ -36,24 +35,32 @@ int main(int argc, char **argv)
 	//乱数生成準備部
 	random_device rd;
 	mt19937 mt(rd());
-	uniform_real_distribution<int> coin(0,1);
+	uniform_int_distribution<int> coin(0,1);
 	uniform_real_distribution<double> score(0.0, 100.0);
 
 	string line1, mapline;
 	string tmp;
 
-	int match_flag = 0; 
+//	int match_flag = 0; 
+	int chr_flag = 0; //現在染色体ペアの0番目か1番目のどちらをコピーしているのか
 //	double sum = 0.0; //shared cMの現時点での合計値
 //	double start = 0.0;
 	double memo = 0.0;
 	double now = 0.0; //現在読んでいる行のmapの値
 	int cnt = 0; //inputファイルの難行目について扱ってるのかcounter
 
-	array<int, 200> chr; //中身は0で初期化されている
+	int chr[200]; //中身は0で初期化されている
 	//左なら0右なら1
+	for(int i=0; i<200; i++){
+		chr[i]=0;
+	}
 
+	//染色体ペアのどちらからコピーを始めるかをランダムに選択	
+	chr[0] = coin(mt);
+	chr_flag = chr[0];
+	cout << "chr[0]=" << chr[0] << endl; 
 
-	chr[0]
+	
 
 	//vector<string> items;
 	while (getline(p1, line1)){
@@ -82,61 +89,44 @@ int main(int argc, char **argv)
 		}
 		now = stod(mapitems[2]);
 		
+	
 		//遺伝子名がマッチしていないとき終了
 		if(items1[1].compare(mapitems[1])){
 			cerr << "mismatch with map line" << endl;
 			exit(1);
 		}
 
-		if(abs(stoi(items1[8])-stoi(items2[8]))!=2){
-			//cout << "match" << ' '  << items1[5] << ' ' << items2[5] << endl;
-			if(!match_flag){//初めて一致したとこ
-				start = stod(mapitems[2]);
-//				cout << "start -- " << start << endl;
-				match_flag = 1;
-			}
-		}else{
-			//cout << items1[5] << ' ' << items2[5] << endl;
-			//if(match_flag){//一致がおわったとこ
-			//end = stod(mapitems[2]);
-			if(match_flag){
-//				cout << "end ---- " << now << endl;
-//				cout << "++++++++++++++++++++++++++++end - start " << now - start << endl;
-				if(now - start > 1){
-					sum += now;
-					sum -= start;
-					cout << "start / now / sum : " << start << " / " << now << " / " << sum << endl; 
-					for(int i=(int)start+1; i<=(int)now; i++){
-						chr[i]=1;
-					}
+
+		//100遺伝子に一度組み換えするかどうかランダムに決める
+		if(!cnt%100){
+			if(score(mt) < now-memo){
+				if(chr_flag==0){
+					chr_flag=1;
+				}else{
+					chr_flag=0;
 				}
-				match_flag = 0;
 			}
+
+			memo = now;
 		}
-		
 
 
+		//chr_flagの通りに遺伝子情報を出力
+		if(chr_flag == 0){
+			cout << items1[0] << ' ' << items1[1] << ' ' << items1[2] << ' '
+				<< items1[5].substr(0,1) << ' ' << items1[6] << ' ' << 0 << endl;
+		}else{
+			cout << items1[0] << ' ' << items1[1] << ' ' << items1[2] << ' '
+				<< items1[5].substr(2,1) << ' ' << items1[7] << ' ' << 0 << endl;
+		}
 
-		
 	}
 
-	//cout << "end  " << now << "  start  " << start << endl;
-	if(match_flag){
-		if(now - start > 1){
-			sum += now;
-			sum -= start;
-			cout << "start / now / sum : " << start << " / " << now << " / " << sum << endl;
-			for(int i=(int)start+1; i<=(int)now; i++){
-				chr[i]=1;
-			}
-			
-		}
-	}
 
-	
 
-		
 
+
+	//組み換えを可視化
 	for(int i=0; i<=int(now); i++){
 		if(chr[i]){
 			cout << "X" ;
