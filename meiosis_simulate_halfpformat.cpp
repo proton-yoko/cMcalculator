5420aa8be95fb673791cf76d5b5984bbd89a3842
proton <whitedew.kilau@gmail.com>
2019-01-04 10:42:17 +0900

    modified format

--- meiosis_simulate_halfpformat.cpp
+++ meiosis_simulate_halfpformat.cpp
@@ -41,37 +41,21 @@ int main(int argc, char **argv)
 	string line1, mapline;
 	string tmp;
 
-//	int match_flag = 0; 
 	int chr_flag = 0; //現在染色体ペアの0番目か1番目のどちらをコピーしているのか
-//	double sum = 0.0; //shared cMの現時点での合計値
-//	double start = 0.0;
 	double memo = 0.0;
 	double now = 0.0; //現在読んでいる行のmapの値
 	int cnt = 0; //inputファイルの難行目について扱ってるのかcounter
 
-//	int chr[200]; //中身は0で初期化されている
-	//左なら0右なら1
-//	for(int i=0; i<200; i++){
-//		chr[i]=0;
-//	}
-
-	//染色体ペアのどちらからコピーを始めるかをランダムに選択	
 	chr_flag = coin(mt);
-//	chr[0] = coin(mt);
-//	chr_flag = chr[0];
-//	cout << "chr[0]=" << chr[0] << endl; 
-
 	
 
 	//vector<string> items;
 	while (getline(p1, line1)){
 		cnt++;
-		//getline(p2, line2);
 
 		//最初のコメント部はスキップ
 		if(line1[0]=='#'){
 			cout << line1 << endl;
-		//	cnt--;
 			continue;
 		}
 		
@@ -101,14 +85,8 @@ int main(int argc, char **argv)
 
 		//100遺伝子に一度組み換えするかどうかランダムに決める
 		if(cnt%5000 == 0){
-//			for(int i=(int)memo+1; i<=(int)now; i++){
-//				chr[i]=chr_flag;
-//			}
-		//	cout << "cnt " << cnt << " memo now " << memo << " " << now << endl;
 			double random_score = score(mt);
-		//	cout << "random " << random_score << " now-memo " << now-memo << endl;
 			if(random_score < now-memo){
-//				cout << "------------------------kumikae" << endl;
 				if(chr_flag==0){
 					chr_flag=1;
 				}else{
@@ -124,10 +102,10 @@ int main(int argc, char **argv)
 
 		//chr_flagの通りに遺伝子情報を出力
 		if(chr_flag == 0){
-			cout << items1[0] << ' ' << items1[1] << ' ' << items1[2] << ' '
+			cout << items1[0] << ' ' << items1[1] << ' ' << items1[2] << ' ' << items1[3] << ' ' << items1[4] << ' ' 
 				<< items1[5].substr(0,1) << ' ' << items1[6] << ' ' << 0 << endl;
 		}else{
-			cout << items1[0] << ' ' << items1[1] << ' ' << items1[2] << ' '
+			cout << items1[0] << ' ' << items1[1] << ' ' << items1[2] << ' ' << items1[3] << ' ' << items1[4] << ' '
 				<< items1[5].substr(2,1) << ' ' << items1[7] << ' ' << 1 << endl;
 		}
 
@@ -135,21 +113,5 @@ int main(int argc, char **argv)
 	
 	}
 
-/*
-	for(int i=(int)memo+1; i<=(int)now; i++){
-		chr[i]=chr_flag;
-	}
-*/
-/*
-	//組み換えを可視化
-	for(int i=0; i<=int(now); i++){
-		if(chr[i]){
-			cout << "X" ;
-		}else{
-			cout << "-" ;
-		}
-	}
-	cout << endl;
-*/
 	return 0;
 }
